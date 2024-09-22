74ac5581fa908d0c05f5ba67ade1600aaace0018
linnsheng <linnsheng2002@gmail.com>
2024-09-22 23:20:46 +0800

    added new stujff

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -67,20 +67,24 @@ int merge_sort(std::vector<int>& arr, int left, int right)
 int insertion_sort_hybrid(std::vector<int>& arr, int left, int right)
 {
     int keyComparison = 0;
-    
-    for (int i = left; i <= right; i++)
+
+    for (int i = left + 1; i <= right; i++)
     {
         int j = i;
-        while (j > left && arr[j - 1] > arr[j])
+        while (j > left)
         {
             keyComparison++;
-            std::swap(arr[j - 1], arr[j]);
-            j--;
+            if (arr[j - 1] > arr[j])
+            {
+                std::swap(arr[j - 1], arr[j]);
+                j--;
+            }
+            else break;
         }
     }
-    
     return keyComparison;
 }
+
 int hybrid_sort(std::vector<int>& arr, int left, int right, int S)
 {
     if (right - left <= S) return insertion_sort_hybrid(arr, left, right);
@@ -112,24 +116,81 @@ void writeOutput1(std::string filename, std::vector<int>& dataSizes, std::vector
     outputFile.close();
 }
 
-void writeOutput2(std::string filename, std::vector<int>& thresholdVals, std::vector<int>& keyComparisons, std::vector<double>& timeTakens)
-{
-    std::ofstream outputFile(filename);
+// void writeOutput2(std::string filename, std::vector<int>& thresholdVals, std::vector<int>& keyComparisons, std::vector<double>& timeTakens)
+// {
+//     std::ofstream outputFile(filename);
 
-    outputFile << "Threshold Value,Key Comparison,Time Taken(ms)" << std::endl;
-    for (int i = 0; i < thresholdVals.size(); i++)
-        outputFile << thresholdVals[i] << "," << keyComparisons[i] << "," << timeTakens[i] << std::endl;
+//     outputFile << "Threshold Value,Key Comparison,Time Taken(ms)" << std::endl;
+//     for (int i = 0; i < thresholdVals.size(); i++)
+//         outputFile << thresholdVals[i] << "," << keyComparisons[i] << "," << timeTakens[i] << std::endl;
     
-    outputFile.close();
-}
-void question_b_i(const std::map<int, std::vector<int>>& dataSets)
+//     outputFile.close();
+// }
+// void question_b_i(const std::map<int, std::vector<int>>& dataSets)
+// {
+//     const int THRESHOLD = 10;
+//     std::vector<int> x_inputSize, y_KeyComparisons;
+//     std::vector<double> timeTaken;
+    
+//     for (const auto&[dataSize, dataSet] : dataSets)
+//     {
+//         std::vector<int> copyDataSet = dataSet;
+//         auto start_timestamp = std::chrono::high_resolution_clock::now();        
+//         auto keyComparisons = hybrid_sort(copyDataSet, 0, copyDataSet.size() - 1, THRESHOLD);
+//         auto end_timestamp = std::chrono::high_resolution_clock::now();        
+//         x_inputSize.emplace_back(dataSize);
+//         y_KeyComparisons.emplace_back(keyComparisons);        
+//         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_timestamp - start_timestamp);
+//         timeTaken.emplace_back(duration.count());       
+//     }
+
+//     writeOutput1("new_output1.csv", x_inputSize, y_KeyComparisons, timeTaken);    
+// }
+
+// void question_b_ii(const std::map<int, std::vector<int>>& dataSets)
+// {
+//     const int SIZE = 1000000;
+    
+//     int initial_thresholdvalue = 1;
+//     std::vector<int> x_thresholdval, y_KeyComparisons;
+//     std::vector<double> timeTaken;
+    
+//     for (int i = initial_thresholdvalue; i < 126; i++)
+//     {
+//         std::vector<int> copyDataSet = dataSets.at(SIZE);
+//         auto start_timestamp = std::chrono::high_resolution_clock::now();        
+//         auto keyComparisons = hybrid_sort(copyDataSet, 0, copyDataSet.size() - 1, i);
+//         auto end_timestamp = std::chrono::high_resolution_clock::now();        
+//         x_thresholdval.emplace_back(i);
+//         y_KeyComparisons.emplace_back(keyComparisons);        
+//         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_timestamp - start_timestamp);
+//         timeTaken.emplace_back(duration.count());           
+//     }
+    
+//     writeOutput2("new_output2.csv", x_thresholdval, y_KeyComparisons, timeTaken);    
+// }
+
+// void question_b_iii(const std::map<int, std::vector<int>>& dataSets)
+// {
+//     // the size we choosing is 1,000,000 -> 5,000,000
+//     int starting_size = 1000000;
+    
+//     for (int i = 1; i <= 5; i++)
+//     {
+//         std::vector<int> copyDataSet = dataSets.at(starting_size * i);
+//         auto mergeC
+//     }
+// }
+
+void GenerateTime(const std::map<int, std::vector<int>>& dataSets)
 {
-    const int THRESHOLD = 10;
+    const int THRESHOLD = 13;
     std::vector<int> x_inputSize, y_KeyComparisons;
     std::vector<double> timeTaken;
     
     for (const auto&[dataSize, dataSet] : dataSets)
     {
+        if (dataSize > 1000000) break;
         std::vector<int> copyDataSet = dataSet;
         auto start_timestamp = std::chrono::high_resolution_clock::now();        
         auto keyComparisons = hybrid_sort(copyDataSet, 0, copyDataSet.size() - 1, THRESHOLD);
@@ -140,42 +201,24 @@ void question_b_i(const std::map<int, std::vector<int>>& dataSets)
         timeTaken.emplace_back(duration.count());       
     }
 
-    writeOutput1("output1.csv", x_inputSize, y_KeyComparisons, timeTaken);    
-}
+    writeOutput1("time_hybrid.csv", x_inputSize, y_KeyComparisons, timeTaken);   
+    x_inputSize.clear(); y_KeyComparisons.clear(); timeTaken.clear();
 
-void question_b_ii(const std::map<int, std::vector<int>>& dataSets)
-{
-    const int SIZE = 1000000;
-    
-    int initial_thresholdvalue = 1;
-    std::vector<int> x_thresholdval, y_KeyComparisons;
-    std::vector<double> timeTaken;
-    
-    for (int i = initial_thresholdvalue; i < 126; i++)
+    for (const auto&[dataSize, dataSet] : dataSets)
     {
-        std::vector<int> copyDataSet = dataSets.at(SIZE);
+        if (dataSize > 1000000) break;
+        std::vector<int> copyDataSet = dataSet;
         auto start_timestamp = std::chrono::high_resolution_clock::now();        
-        auto keyComparisons = hybrid_sort(copyDataSet, 0, copyDataSet.size() - 1, i);
+        auto keyComparisons = merge_sort(copyDataSet, 0, copyDataSet.size() - 1);
         auto end_timestamp = std::chrono::high_resolution_clock::now();        
-        x_thresholdval.emplace_back(i);
+        x_inputSize.emplace_back(dataSize);
         y_KeyComparisons.emplace_back(keyComparisons);        
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_timestamp - start_timestamp);
-        timeTaken.emplace_back(duration.count());           
+        timeTaken.emplace_back(duration.count());       
     }
-    
-    writeOutput2("output2.csv", x_thresholdval, y_KeyComparisons, timeTaken);    
-}
 
-void question_b_iii(const std::map<int, std::vector<int>>& dataSets)
-{
-    // the size we choosing is 1,000,000 -> 5,000,000
-    int starting_size = 1000000;
-    
-    for (int i = 1; i <= 5; i++)
-    {
-        std::vector<int> copyDataSet = dataSets.at(starting_size * i);
-        auto mergeC
-    }
+    writeOutput1("time_merge.csv", x_inputSize, y_KeyComparisons, timeTaken);   
+
 }
 int main()
 {
@@ -191,18 +234,18 @@ int main()
 
     // Generating dataset
     for (const auto& dataSize : dataSizes)
-        dataSets[dataSize] = generateDataSet(dataSize, 100);
+        dataSets[dataSize] = generateDataSet(dataSize, 1000);
 
     // b) i)
     // Setting Threshold to 10
-    question_b_i(dataSets);
+    // question_b_i(dataSets);
     
     // b) ii)
     // Set N to 1,000,000
-    question_b_ii(dataSets);
+    // question_b_ii(dataSets);
     
     // c)
-    question_b_iii(dataSets);
-    
+    // question_b_iii(dataSets);
+    GenerateTime(dataSets);    
     return 0;
 }
